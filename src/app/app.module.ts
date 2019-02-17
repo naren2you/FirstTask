04328353df52cb27079d1799c7946ebce52e5727
Narendra <naren2you@gmail.com>
2019-02-17 21:41:46 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './page/profile/profile.component';
+import { NgXBootstrapModule } from "./ngxbootstrap";
+import { NavigationComponent } from "./page/navigation/navigation.component";
+import { UserListComponent } from "./page/user/user-list/user-list.component";
+import { UserAddComponent } from "./page/user/user-add/user-add.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    NavigationComponent,
+    UserListComponent,
+    UserAddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgXBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
